cfca6706073c4a11ce220ea6009f73b2f9096145
linlin200217 <61957900+linlin200217@users.noreply.github.com>
2024-02-22 20:32:56 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,47 @@
+import openai
+import csv
+from openai import OpenAI
+
+OPENAI_API_KEY = "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"
+
+system_prompt = ("You will be provided with a csv data file. Analyse the data. First figure out the data subject, and based on the subject, associate several relevent objects.\n"
+                "Requirement: At least generate 8 objects. They MUST BE all nouns which are highly pertinent to the data. They MUST BE physical objects that can be portrayed in a picture. DO NOT simply copy the attributes or values in the data.\n"
+                "Example:\n"
+                "User Input: Here is the data from the CSV file:\n"
+                "- Name: McDonald, Meat: Beef, Bread: White, Vegetable: Tomato, Healthy Deg: 1, Popular Deg: 3\n"
+                "- Name: KFC, Meat: Chicken, Bread: Wheat, Vegetable: Cucumber, Healthy Deg: 3, Popular Deg: 2\n"
+                "- Name: BurgerKing, Meat: Beef, Bread: Wheat, Vegetable: Cucumber, Healthy Deg: 3, Popular Deg: 1\n"
+                "- Name: KFC, Meat: Chicken, Bread: Wheat, Vegetable: Tomato, Healthy Deg: 4, Popular Deg: 2\n"
+                "- Name: BurgerKing, Meat: Beef, Bread: Wheat, Vegetable: Tomato, Healthy Deg: 2, Popular Deg: 1\n"
+                "- Name: McDonald, Meat: Beef, Bread: Honey, Vegetable: Cucumber, Healthy Deg: 1, Popular Deg: 3\n"
+                "Your answer:\n"
+                "hamburger, sandwich, chip, fried chicken, potato, tomato, pizza, restaurant"
+)
+
+def csv_to_text(data_path):
+    data = []
+    with open(data_path, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    text_content = "Here is the data from the CSV file:\n"
+    for row in data:
+        row_text = ', '.join([f"{key}: {value}" for key, value in row.items()])
+        text_content += f"- {row_text}\n"
+
+    return text_content
+
+def word_recommendation(system_prompt, data_path):
+    client = OpenAI(
+    api_key = OPENAI_API_KEY,
+    )
+    chat_completion = client.chat.completions.create(
+        messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": csv_to_text(data_path)}
+        ],
+        model="gpt-4",
+    )
+    word_cloud_str = chat_completion.choices[0].message.content
+    word_cloud_lst = [item.strip() for item in word_cloud_str.split(',')]
+    return word_cloud_lst
