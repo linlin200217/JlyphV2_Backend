31986a014b7ec4851ca5cce9d487192b43f7b23e
linlin200217 <61957900+linlin200217@users.noreply.github.com>
2024-02-22 21:14:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from utils import DATAPATH, data_process, word_recommendation
 
 app = Flask(__name__)
 CORS(app)
@@ -7,3 +8,20 @@ CORS(app)
 @app.route('/')
 def hello():
     return 'Backend listening...'
+
+@app.route("/upload", methods=["POST"])
+async def load_data():
+    """
+    POST FILE
+    return json data {
+        data_title: string,
+        Categorical: [],
+        Numerical: [],
+    }
+    """
+    if request.method == 'POST':
+        f = request.files['file']
+        file_path = os.path.join(DATAPATH, secure_filename(f.filename))
+        f.save(file_path)
+        struct = data_process(file_path)
+        return jsonify(struct)
