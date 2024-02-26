58d955ab5d1a434adfaf876d654aadea29b3f43c
linlin200217 <61957900+linlin200217@users.noreply.github.com>
2024-02-27 00:58:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,38 @@ async def pregenerate():
     image_ids = image_recommendation(request.json["user_prompt"])
     return jsonify({"status": "success", "image_id": image_ids})
 
+@app.route("/maskselect", methods=["POST"])
+async def maskselect():
+    """
+    INPUT DATA{
+        widget: {'x': 162, 'y': 279, 'width': 204, 'height': 58}
+        image_id: str
+    }
+    RETURN DATA {
+        mask_image_id:str,
+        outlier_image_id:str
+    }
+    """
+    return None
+
+async def generate_element():
+    """
+    INPUT DATA{
+        masks: {
+            'Meat': [widget_meet,2], 
+            'Vegetable': [widget_tomato,2],
+            'Name': [widget_bottom_bread,2],
+            'Bread': [widget_top_bread,2]
+            }
+        image_id: str
+    }
+    RETURN DATA {
+        {'Meat':[{'rgba_image_id': 'rgba1708965745', 'prompt_detail': 'Beef'}, {'rgba_image_id': 'rgba1708963415', 'prompt_detail': 'Chicken'}],...}
+    }
+    """
+    return None
+
+@app.route("/image/<image_id>")
 def get_image(image_id):
     """
     GET: HOST/image/<image_id> 
